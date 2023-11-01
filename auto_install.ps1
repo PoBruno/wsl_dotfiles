3d0d2a0945339535e783a845a1a05e78e21d64f3
Bruno Gomes <brunoshy@gmail.com>
2023-11-01 04:06:05 +0000

    update script auto_config.ps1

--- auto_install.ps1
+++ auto_install.ps1
@@ -17,30 +17,49 @@ Write-Host "----------------Installing Scoop----------------"
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
 Write-Host "----------------Installing All The Apps----------------"
-winget install -e --id Microsoft.VCRedist.2015+.x64
-winget install -e --id Microsoft.VCRedist.2015+.x86
-winget install -e --id Microsoft.VCRedist.2013.x64
-winget install -e --id Microsoft.VCRedist.2013.x86
-winget install -e --id Microsoft.DotNet.Runtime.6
-winget install -e --id Microsoft.DirectX
-winget install -e --id 7zip.7zip
-winget install -e --id Git.Git
-winget install -e --id VideoLAN.VLC
-winget install -e --id Notepad++.Notepad++
-winget install -e --id Google.Chrome
-winget install -e --id Zoom.Zoom
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Starship.Starship
-winget install -e --id Microsoft.PowerToys
-winget install -e --id EpicGames.EpicGamesLauncher
-winget install -e --id Valve.Steam
-winget install -e --id Docker.DockerDesktop
-winget install -e --id Discord.Discord
-winget install -e --id OBSProject.OBSStudio
-winget install -e --id Google.NearbyShare
-winget install 9NKSQGP7F2NH
-scoop install lsd
-scoop install winfetch
+winget install -e --id AgileBits.1Password --silent --accept-source-agreements
+winget install -e --id Nvidia.GeForceExperience --silent --accept-source-agreements
+winget install -e --id Nvidia.PhysX --silent --accept-source-agreements
+winget install -e --id Valve.Steam --silent --accept-source-agreements
+winget install -e --id Google.Chrome --silent --accept-source-agreements
+winget install -e --id Notepad++.Notepad++ --silent --accept-source-agreements
+winget install -e --id Discord.Discord --silent --accept-source-agreements
+winget install -e --id EpicGames.EpicGamesLauncher --silent --accept-source-agreements
+winget install -e --id Obsidian.Obsidian --silent --accept-source-agreements
+winget install -e --id 7zip.7zip --silent --accept-source-agreements
+winget install -e --id RARLab.WinRAR --silent --accept-source-agreements
+winget install -e --id Git.Git --silent --accept-source-agreements
+winget install -e --id AltDrag.AltDrag --silent --accept-source-agreements
+winget install -e --id Microsoft.Edge --silent --accept-source-agreements
+winget install -e --id Microsoft.WindowsTerminal --silent --accept-source-agreements
+winget install -e --id Microsoft.Office --silent --accept-source-agreements
+winget install -e --id OBSProject.OBSStudio --silent --accept-source-agreements
+winget install -e --id Postman.Postman --silent --accept-source-agreements
+winget install -e --id RaspberryPiFoundation.RaspberryPiImager --silent --accept-source-agreements
+winget install -e --id Stremio.Stremio --silent --accept-source-agreements
+winget install -e --id qBittorrent.qBittorrent --silent --accept-source-agreements
+winget install -e --id Fortinet.FortiClientVPN --silent --accept-source-agreements
+winget install -e --id Hashicorp.Terraform --silent --accept-source-agreements
+winget install -e --id Microsoft.AzureCLI --silent --accept-source-agreements
+winget install -e --id GitHub.cli --silent --accept-source-agreements
+winget install -e --id Microsoft.PowerShell --silent --accept-source-agreements
+winget install -e --id PuTTY.PuTTY --silent --accept-source-agreements
+winget install -e --id Starship.Starship --silent --accept-source-agreements
+winget install -e --id Microsoft.VisualStudioCode --silent --accept-source-agreements
+winget install -e --id Transmission.Transmission --silent --accept-source-agreements
+winget install -e --id VideoLAN.VLC --silent --accept-source-agreements
+winget install -e --id TechSmith.Snagit.2021 --silent --accept-source-agreements
+winget install -e --id Microsoft.VCRedist.2015+.x64 --silent --accept-source-agreements
+winget install -e --id Microsoft.VCRedist.2015+.x86 --silent --accept-source-agreements
+winget install -e --id Microsoft.VCRedist.2013.x64 --silent --accept-source-agreements
+winget install -e --id Microsoft.VCRedist.2013.x86 --silent --accept-source-agreements
+winget install -e --id Microsoft.DotNet.Runtime.6 --silent --accept-source-agreements
+winget install -e --id Microsoft.DirectX --silent --accept-source-agreements
+winget install -e --id Microsoft.PowerToys --silent --accept-source-agreements
+winget install -e --id Zoom.Zoom --silent --accept-source-agreements
+winget install 9NKSQGP7F2NH --silent --accept-source-agreements
+scoop install lsd --silent --accept-source-agreements
+scoop install winfetch --silent --accept-source-agreements
 
 Write-Host "----------------Restore Legacy Context Menu----------------"
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
